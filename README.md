d8a5b7ed0f7f8e8e3a665cc8790891f3c11dd37a
向文可 <1041367524@qq.com>
2023-05-12 10:59:55 +0800

    feat: eslint shared config

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-#eslint-config
+# ESLint Config
+# 介绍
+
+> ESLint Shared Config With TypeScript And Vue
+
+# 安装
+
+```
+npm install --save-dev @saatana97/eslint-config
+pnpm add --save-dev @saatana97/eslint-config
+```
+
+# 使用
+
+```typescript
+// .eslintrc
+{
+    "extends": "@saatana97/eslint-config"
+}
+// in vue projects
+{
+    "extends": "@saatana97/eslint-config/vue"
+}
+```
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+import { resolve } from 'node:path';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { type ConfigEnv, type UserConfig } from 'vite';
+import packageJson from './package.json';
+const resolvePath = (str: string) => resolve(__dirname, str);
+// https://vitejs.dev/config/
+export default (configEnv: ConfigEnv) => {
+    console.info(configEnv);
+    return {
+        plugins: [
+            visualizer({
+                open: false,
+                gzipSize: true,
+            }),
+        ],
+        build: {
+            sourcemap: false,
+            lib: {
+                formats: ['cjs'],
+                fileName: (format, entryName) => `${entryName}.${format}`,
+                entry: [resolvePath('./src/index.ts'), resolvePath('./src/vue.ts')],
+                name: packageJson.name.replace(/-(\w)/gi, (_, v) => v.toUpperCase()),
+            },
+        },
+    } as UserConfig;
+};
