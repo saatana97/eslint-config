153b261709dbeff96756939a447cabe4051710d1
向文可 <13202090601@163.com>
2023-12-05 16:01:01 +0800

    revert: split core

--- packages/eslint-config/src/ts.ts
+++ packages/eslint-config/src/ts.ts
@@ -1,1 +1,41 @@
-export { ts as default } from '@pkgs/core';
+export default {
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked',
+        'plugin:prettier/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: true,
+    },
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+    },
+    rules: {
+        '@typescript-eslint/consistent-type-imports': 'warn',
+        '@typescript-eslint/no-import-type-side-effects': 'warn',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-misused-promises': 'off',
+        '@typescript-eslint/no-floating-promises': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-argument': 'off',
+        '@typescript-eslint/no-unsafe-return': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
+        'prettier/prettier': 'warn',
+        'no-alert': 'warn',
+        'no-debugger': 'warn',
+        'no-console': 'warn',
+        'no-undef': 'error',
+        'no-else-return': 'error',
+        eqeqeq: [2, 'always'],
+    },
+    globals: {
+        process: 'readonly',
+        arguments: 'readonly',
+    },
+};
